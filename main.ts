16725caa965f88e7db7b348fa93a8889b0853ed9
daisyliu0225 <50540881+daisyliu0225@users.noreply.github.com>
2022-07-22 20:50:21 +0800

    use break

--- main.ts
+++ main.ts
@@ -1,22 +1,11 @@
-let butA = 0
-let butB = 0
-let index = 0
 input.onButtonPressed(Button.A, function () {
     butA = 1
 })
-input.onButtonPressed(Button.B, function () {
-    butB = 1
-})
-basic.forever(function () {
+let butA = 0
+for (let index = 0; index <= 10; index++) {
+    basic.showNumber(index)
+    basic.pause(100)
     if (butA == 1) {
-        basic.showNumber(index)
-        basic.pause(100)
-        index += 1
-        if (butB == 1) {
-            butA = 0
-            butB = 0
-            index = 0
-            basic.clearScreen()
-        }
+        break;
     }
-})
+}
